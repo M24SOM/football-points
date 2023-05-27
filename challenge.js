1fab09a1780617fdee1fcdb40b4d1504ab7d7212
Mohamed Mahad Farah <mmfarah@proton.me>
2023-05-27 12:12:09 +0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,8 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculatePoints(wins, draws, losses) {
+  let totalPoints = (wins * 3) + (draws * 1) + (losses * 0);
+  console.log(totalPoints);
+}
+
+calculatePoints(5, 2, 3); // Output: 17
